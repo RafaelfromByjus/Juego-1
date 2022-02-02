9d979c26f2c4cb2612b32a789f6dfe47150d6ff7
RafaelfromByjus <88950240+RafaelfromByjus@users.noreply.github.com>
2022-02-01 20:25:49 -0800

    Add files via upload

---
+++ Juego (Rafa)/sketch.js
@@ -0,0 +1,23 @@
+
+var hero, villian, queen;
+var floor1;
+var ladder1;
+var fireball1;
+var gameState = 0;
+var score = 0;
+
+
+function setup(){
+    var canvas 
+    canvas = createCanvas(1500, 1500);
+
+    floor1 = createSprites(750, 750, 20, 200);
+    
+}
+function preload(){
+
+}
+function draw(){
+
+    drawSprites();
+}
